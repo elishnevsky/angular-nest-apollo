8eee46a43d9ad9105b1da0117b8f0c2a9d754319
Eugene Lishnevsky <elishnevsky@gmail.com>
2020-02-19 12:37:38 -0500

    Implement basic GraphQL server

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,74 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule, Args } from '@nestjs/graphql';
+import { authors, books } from './data';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+const typeDefs = `
+  type Author {
+    id: ID!
+    name: String!
+    books: [Book]!
+  }
+  input AuthorInput {
+    id: Int!
+    name: String!
+  }
+  type Book {
+    id: ID!
+    title: String!
+    authorId: Int!
+    author: Author!
+  }
+  type Query {
+    authors: [Author]
+    author(id: Int!): Author
+    books: [Book]
+    book(id: Int!): Book
+  }
+  type Mutation {
+    updateAuthor(author: AuthorInput!): Author
+  }
+`;
+
+const resolvers = {
+  Author: {
+    books(author) {
+      return books.filter(book => book.authorId === author.id);
+    }
+  },
+  Book: {
+    author(book) {
+      return authors.find(author => author.id === book.authorId);
+    }
+  },
+  Query: {
+    authors() {
+      return authors;
+    },
+    author(obj, args, context, info) {
+      return authors.find(author => author.id === args.id);
+    },
+    books() {
+      return books;
+    },
+    book(obj, args, context, info) {
+      return books.find(b => b.id === args.id);
+    },
+  },
+  Mutation: {
+    updateAuthor(obg, args) {
+      const author = authors.find(a => a.id === args.author.id);
+      author.name = args.author.name;
+      return author;
+    }
+  }
+};
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [
+    GraphQLModule.forRoot({
+      typeDefs: typeDefs,
+      resolvers: resolvers
+    })
+  ]
 })
 export class AppModule {}
---
+++ apps/server/src/app/data.ts
@@ -0,0 +1,16 @@
+export const authors = [
+  { id: 1, name: 'J. K. Rowling' },
+  { id: 2, name: 'J. R. R. Tolkien' },
+  { id: 3, name: 'Brent Weeks' }
+];
+
+export const books = [
+  { id: 1, title: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+  { id: 2, title: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+  { id: 3, title: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+  { id: 4, title: 'The Fellowship of the Ring', authorId: 2 },
+  { id: 5, title: 'The Two Towers', authorId: 2 },
+  { id: 6, title: 'The Return of the King', authorId: 2 },
+  { id: 7, title: 'The Way of Shadows', authorId: 3 },
+  { id: 8, title: 'Beyond the Shadows', authorId: 3 }
+];
